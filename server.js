5ab7b48ffcd485be0225a43ef82c201fd6e84ea4
Anson Nguyen <anson@enclave.vn>
2023-05-30 13:09:13 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-var db = require("./database.js");
-var cors = require('cors')
+var { db, dbUil } = require("./database.js");
+var cors = require('cors');
+const { request } = require('http');
 const app = express();
 
 
@@ -9,209 +10,134 @@ const app = express();
 app.use(cors());
 
 app.get("/api/sponsors", (req, res, next) => {
-  var sql = `SELECT * FROM Sponsor`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
+  new dbUil().getSponsors().then((rows) => {
     res.json({
       "message": "success",
       "data": rows
     })
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
 app.get("/api/teamsWithoutPlayer", (req, res, next) => {
-  var sql = `SELECT * FROM Team`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
+  new dbUil().getTeamsWithoutPlayer().then((rows) => {
     res.json({
       "message": "success",
       "data": rows
     })
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
 app.get("/api/teams", (req, res, next) => {
-  var sql = `SELECT Player.Id, Player.Name AS PlayerName, TeamId, Team.Name AS TeamName
-                FROM Player
-                JOIN Team
-                ON Player.TeamId = Team.Id
-                ORDER BY TeamId, Player.Name`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
+  new dbUil().getTeams().then((rows) => {
     res.json({
       "message": "success",
       "data": rows
     })
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
 app.get("/api/fixtures", (req, res, next) => {
-  var sql = `SELECT Fixture.Id, TimeLocation, HomeTeamId, HomeTeam.Name AS HomeTeamName, 
-              AwayTeamId, AwayTeam.Name AS AwayTeamName, Result1, Result2
-              FROM "Fixture" 
-              JOIN Team AS HomeTeam 
-              ON HomeTeam.Id = Fixture.HomeTeamId
-              JOIN Team AS AwayTeam 
-              ON AwayTeam.Id = Fixture.AwayTeamId
-              ORDER BY Fixture.Id`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
-    var checkFinal = rows.indexOf(s => s.TimeLocation == "[Final] 7B, 18:30 Thu Jun 22 2023");
-    if (checkFinal == -1) {
-      rows.push({
-        "Id": 99,
-        "TimeLocation": "[Final] 7B, 18:30 Thu Jun 22 2023",
-        "HomeTeamId": null,
-        "HomeTeamName": "1st Group",
-        "AwayTeamId": null,
-        "AwayTeamName": "2nd Group",
-        "Result1": null,
-        "Result2": null
-      });
-    }
+  new dbUil().getFixtures().then((rows) => {
     res.json({
       "message": "success",
       "data": rows
     })
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
+
+
 app.get("/api/fixturesCSV", (req, res, next) => {
-  var sql = `SELECT Fixture.Id, TimeLocation, HomeTeamId, HomeTeam.Name AS HomeTeamName, 
-              AwayTeamId, AwayTeam.Name AS AwayTeamName, Result1, Result2
-              FROM "Fixture" 
-              JOIN Team AS HomeTeam 
-              ON HomeTeam.Id = Fixture.HomeTeamId
-              JOIN Team AS AwayTeam 
-              ON AwayTeam.Id = Fixture.AwayTeamId
-              ORDER BY Fixture.Id`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
-    var checkFinal = rows.indexOf(s => s.TimeLocation == "[Final] 7B, 18:30 Thu Jun 22 2023");
-    if (checkFinal == -1) {
-      rows.push({
-        "Id": 99,
-        "TimeLocation": "[Final] 7B, 18:30 Thu Jun 22 2023",
-        "HomeTeamId": null,
-        "HomeTeamName": "1st Group",
-        "AwayTeamId": null,
-        "AwayTeamName": "2nd Group",
-        "Result1": null,
-        "Result2": null
-      });
-    }
+  new dbUil().getFixtures().then((rows) => {
     let csvData = ["Time Location", "Home Team", "Away Team", "Result"].join(",") + "\r\n"
     rows.forEach((row) => {
-      let result = (row.Result1 ?? '') + ' - ' + (row.Result2  ?? '');
+      let result = (row.Result1 ?? '') + ' - ' + (row.Result2 ?? '');
       csvData += ["\"" + row.TimeLocation + "\"", row.HomeTeamName, row.AwayTeamName, result].join(",") + "\r\n"
     })
     res
-    .set({
-      "Content-Type": "text/csv",
-      "Content-Disposition": `attachment; filename="fixtures.csv"`,
-    })
-    .send(csvData);
+      .set({
+        "Content-Type": "text/csv",
+        "Content-Disposition": `attachment; filename="fixtures.csv"`,
+      })
+      .send(csvData);
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
 app.get("/api/standings", (req, res, next) => {
-  var sql = `SELECT Id, TeamId, Team.Name AS TeamName, Played, Won, 
-              Drawn, Lost, Points, GF, GA, GD
-              FROM "TeamStanding"
-              JOIN Team ON Team.Id = TeamStanding.TeamId
-              ORDER BY Points DESC, Team.Name`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
+  new dbUil().getStandings().then((rows) => {
     res.json({
       "message": "success",
       "data": rows
     })
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
 app.get("/api/standingsCSV", (req, res, next) => {
-  var sql = `SELECT Id, TeamId, Team.Name AS TeamName, Played, Won, 
-              Drawn, Lost, Points, GF, GA, GD
-              FROM "TeamStanding"
-              JOIN Team ON Team.Id = TeamStanding.TeamId
-              ORDER BY Points DESC, Team.Name`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
-    res.json({
-      "message": "success",
-      "data": rows
+  new dbUil().getStandings().then((rows) => {
+    let csvData = ["Team Name", "Played", "Drawn", "Won", "Lost", "Points", "GF", "GA", "GD"].join(",") + "\r\n"
+    rows.forEach((row) => {      
+      csvData += [row.TeamName, row.Played, row.Drawn, row.Won, row.Lost, row.Points, row.GF, row.GA, row.GD].join(",") + "\r\n"
     })
+    res
+      .set({
+        "Content-Type": "text/csv",
+        "Content-Disposition": `attachment; filename="standings.csv"`,
+      })
+      .send(csvData);
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
+
 app.get("/api/scorers", (req, res, next) => {
-  var sql = `SELECT PlayerId, Player.Name, SUM(Goals) AS TotalGoals FROM Scorer
-                JOIN Player ON Player.Id = Scorer.PlayerId
-                GROUP BY PlayerId
-                ORDER BY TotalGoals DESC`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
+  new dbUil().getScorers().then((rows) => {
     res.json({
       "message": "success",
       "data": rows
     })
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
 });
 
 app.get("/api/scorersCSV", (req, res, next) => {
-  var sql = `SELECT PlayerId, Player.Name, SUM(Goals) AS TotalGoals FROM Scorer
-                JOIN Player ON Player.Id = Scorer.PlayerId
-                GROUP BY PlayerId
-                ORDER BY TotalGoals DESC`;
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
+  new dbUil().getFixtures().then((rows) => {
     let csvData = '';
     rows.forEach((row) => {
       csvData += [row.Name, row.TotalGoals].join(",") + "\r\n"
     })
     res
-    .set({
-      "Content-Type": "text/csv",
-      "Content-Disposition": `attachment; filename="scorers.csv"`,
-    })
-    .send(csvData);
+      .set({
+        "Content-Type": "text/csv",
+        "Content-Disposition": `attachment; filename="scorers.csv"`,
+      })
+      .send(csvData);
+  }, (err) => {
+    res.status(400).json({ "error": err.message });
+    return;
   });
+
 });
 
 app.listen(3000, () => {
